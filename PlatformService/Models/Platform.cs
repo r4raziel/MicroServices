7a101df4ff42aa712bbf3e95ac21444252cb1dd9
VxTredge <iv.raziel@gmail.com>
2022-09-15 11:02:37 -0500

    First platform model with solution added

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,14 @@
+namespace PaltformService.Models
+{
+
+    public class Paltform
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public int Publisher { get; set; }
+        public int Cost { get; set; }
+
+    }
+
+}
