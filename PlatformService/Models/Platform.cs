084d57a733703d40aac711a69667685b658957d3
VxTredge <iv.raziel@gmail.com>
2022-09-16 03:53:37 -0500

    adding data annotations

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PaltformService.Models
 {
 
     public class Paltform
     {
+        [Key]
         public int Id { get; set; }
         public int Name { get; set; }
-
         public int Publisher { get; set; }
         public int Cost { get; set; }
 
