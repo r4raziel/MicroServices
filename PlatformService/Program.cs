fdfe0261fbb7c6d80d0a2e1aceed4c665bb8a3c9
silyas@pattersoncompanies.com <silyas@pattersoncompanies.com>
2022-09-28 02:17:37 +0500

    !success production running on SQLSERVER image on ubuntu Linux...

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+if (builder.Environment.IsProduction())
+ { 
+    var sqlConBuilder = new SqlConnectionStringBuilder();
+    sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("PlatformsConnection");
+    Console.WriteLine("---> using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
+}
+else
+{
+  Console.WriteLine("---> using InMem Db");
+  builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+}
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,8 +27,9 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
-var app = builder.Build();
 
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -33,6 +45,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-PrepDb.PrepPopulation(app);
+Console.WriteLine($"---app is running on: {app.Environment.EnvironmentName} ");
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
